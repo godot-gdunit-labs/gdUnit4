c00b599339e97de530305905cd0cbcb1d7ed703c
Nullpointer <mikeschulze.lpz@gmail.com>
2026-04-28 09:44:20 +0200

    fix parser tests

--- addons/gdUnit4/test/core/parse/GdScriptParserTest.gd
+++ addons/gdUnit4/test/core/parse/GdScriptParserTest.gd
@@ -458,22 +458,25 @@ func test_strip_leading_spaces() -> void:
 func test_parse_func_description() -> void:
 	var script := GdScriptTestHelper.build_tmp_script("""
 
-		@warning_ignore("untyped_declaration")
-		func foo0():
-			pass
+		@warning_ignore("unused_parameter")
+		func foo0(arg1: int, arg2: bool=false) -> String:
+			return ""
 
 		@warning_ignore("unused_parameter")
-		static func foo1(arg1: int, arg2: bool =false) -> String:
+		static func foo1(arg1: int, arg2: bool=false) -> String:
 			return ""
 
 		@warning_ignore("untyped_declaration", "unused_parameter")
-		static func foo2(arg1: int, arg2: bool =true):
-			pass
+		static func foo2(arg1: int, arg2: bool=true):
+			return
 	""")
 	var fds := _parser.get_function_descriptors(script)
 
 	assert_that(fds[0])\
-		.is_equal(GdFunctionDescriptor.create("foo0", script.resource_path, 4, GdObjects.TYPE_VOID))
+		.is_equal(GdFunctionDescriptor.create("foo0", script.resource_path, 4, TYPE_STRING, [
+			GdFunctionArgument.new("arg1", TYPE_INT),
+			GdFunctionArgument.new("arg2", TYPE_BOOL, false)
+		]))
 
 	# static function
 	assert_that(fds[1])\
@@ -484,7 +487,7 @@ func test_parse_func_description() -> void:
 
 	# static function without return type
 	assert_that(fds[2])\
-		.is_equal(GdFunctionDescriptor.create_static("foo2", script.resource_path, 12, GdObjects.TYPE_VOID, [
+		.is_equal(GdFunctionDescriptor.create_static("foo2", script.resource_path, 12, GdObjects.TYPE_VARIANT, [
 			GdFunctionArgument.new("arg1", TYPE_INT),
 			GdFunctionArgument.new("arg2", TYPE_BOOL, true)
 		]))
@@ -545,7 +548,9 @@ func test_parse_class_inherits() -> void:
 				GdFunctionArgument.new("arg2", TYPE_INT, 23),
 				GdFunctionArgument.new("name", TYPE_STRING, "test"),
 			]),
-			GdFunctionDescriptor.create("foo5", "res://addons/gdUnit4/test/mocker/resources/CustomResourceTestClass.gd", 17, GdObjects.TYPE_VOID),
+			# see https://github.com/godotengine/godot/pull/118032
+			GdFunctionDescriptor.create("foo5", "res://addons/gdUnit4/test/mocker/resources/CustomResourceTestClass.gd", 17,
+				GdObjects.TYPE_VARIANT if Engine.get_version_info()["minor"] >= 7 else GdObjects.TYPE_VOID),
 		])
 
 
@@ -671,8 +676,10 @@ func test_parse_func_descriptor_with_fuzzers() -> void:
 			pass
 	""")
 	var fds := _parser.get_function_descriptors(script, ["test_foo"])
+	# see https://github.com/godotengine/godot/pull/118032
+	var untyped_return_type: int = GdObjects.TYPE_VARIANT if Engine.get_version_info()["minor"] >= 7 else GdObjects.TYPE_VOID
 
-	assert_that(fds[0]).is_equal(GdFunctionDescriptor.create("test_foo", script.resource_path, 12, GdObjects.TYPE_VOID, [
+	assert_that(fds[0]).is_equal(GdFunctionDescriptor.create("test_foo", script.resource_path, 12, untyped_return_type, [
 		# all fuzzer must by type TYPE_FUZZER
 		GdFunctionArgument.new("fuzzer_a", GdObjects.TYPE_FUZZER, "fuzz_a()"),
 		GdFunctionArgument.new("fuzzer_b", GdObjects.TYPE_FUZZER, "fuzz_b()"),

a1dd5c44d067142090a1f99aaeb7ff190c9c50f5
Nullpointer <mikeschulze.lpz@gmail.com>
2026-07-22 19:56:49 +0200

    Update addons/gdUnit4/test/core/command/GdUnitCommandTestSessionTest.gd

--- addons/gdUnit4/test/core/command/GdUnitCommandTestSessionTest.gd
+++ addons/gdUnit4/test/core/command/GdUnitCommandTestSessionTest.gd
@@ -23,11 +23,25 @@ func test_runner_process_id_initialized_to_invalid() -> void:
 	assert_int(_command._current_runner_process_id).is_equal(-1)
 
 
-func test_is_valid_runner_process_rejects_default_pid() -> void:
+func test__is_valid_runner_process_false() -> void:
+	assert_bool(GdUnitCommandTestSession._is_valid_runner_process(-1)).is_false()
+	
 	# `0` is the uninitialized/debug-mode value and must never be treated as a killable process.
 	assert_bool(GdUnitCommandTestSession._is_valid_runner_process(0)).is_false()
+	
+func test__is_valid_runner_process_true() -> void:	
+	var args: PackedStringArray
+	
+	if OS.get_name() == "Windows":
+		args = ["-n", "5", "127.0.0.1"]
+	else:
+		args = ["-c", "5", "127.0.0.1"]
 
+	var pid := OS.create_process("ping", args, false)
 
-func test_is_valid_runner_process_rejects_negative_pid() -> void:
-	assert_bool(GdUnitCommandTestSession._is_valid_runner_process(-1)).is_false()
+	assert_bool(_is_valid_runner_process(pid)).is_true()
+
+	# cleanup
+	if OS.is_process_running(pid):
+		OS.kill(pid)
 #endregion
